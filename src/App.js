79ecf1e23b39bcab77af871f87b7d02e1310823d
Sophie <50250729+hellosophiee@users.noreply.github.com>
2021-06-02 12:33:55 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import TodoContainer from "./components/TodoContainer";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div>
+      <TodoContainer />
+    </div>
+  );
+}
