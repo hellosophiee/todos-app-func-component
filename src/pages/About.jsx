9f98a1cd7c9ceda7cf871fc40688539236216e39
Sophie <50250729+hellosophiee@users.noreply.github.com>
2021-06-02 14:54:17 +0500

    feat: add Hamburger menu

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,11 +3,11 @@ import { Link, Route, useRouteMatch } from "react-router-dom";
 import SinglePage from "./SinglePage";
 
 const About = () => {
-  // console.log(useRouteMatch());
   const { url, path } = useRouteMatch();
+
   return (
-    <div>
-      <ul>
+    <div className="about__content">
+      <ul className="about__list">
         <li>
           <Link to={`${url}/about-app`}>About App</Link>
         </li>
